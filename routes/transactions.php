ad2d3b3a20c1eee6da35586200e7d7c8043bbf3c
Chris Hartjes <chartjes@yggdrasil.local>
2012-03-02 10:08:03 -0500

    Added in route for getting archived transactions

--- routes/transactions.php
+++ routes/transactions.php
@@ -4,6 +4,11 @@
  * All routes dealing with returning transaction information
  */
 
+$app->get('/transactions/archived', function() use ($container) {
+    $transaction = new \IBL\Transaction($container['db_connection']);
+    echo $transaction->getArchived();
+});
+
 $app->get('/transactions/current', function() use ($container) {
     $transaction = new \IBL\Transaction($container['db_connection']);
     echo $transaction->getCurrent();
